14a18737eaf4019b43f54438768bba53d37768a2
nikkig124 <nikkig124@gmail.com>
2021-01-10 10:40:55 -0600

    add env var to auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,8 +6,12 @@ const start = async () => {
         throw new Error('JWT_KEY is undefined');
     }
 
+    if (!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI is undefined');
+    }
+
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
