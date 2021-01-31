0aa0712e2ebf4f5a31f6904f4459a9907507c663
nikkig124 <nikkig124@gmail.com>
2021-01-31 10:26:32 -0600

    payments service set up

---
+++ payments/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@ng-tickets/common';
+// import { createTicketRouter } from './routes/new';
+// import { getTicketRouter } from './routes/show';
+// import { getTicketsRouter } from './routes/index';
+// import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+// app.use(createTicketRouter);
+// app.use(getTicketRouter);
+// app.use(getTicketsRouter);
+// app.use(updateTicketRouter);
+
+
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
+
